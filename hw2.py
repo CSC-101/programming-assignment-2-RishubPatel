b52c8867df572216c3f1d821f4d94bbc0ba1948a
RishubPatel <rpate133@calpoly.edu>
2024-10-23 09:54:32 -0700

    Implemented functions and tests for parts 1 through 4

--- hw2.py
+++ hw2.py
@@ -4,17 +4,60 @@ import data
 
 # Part 1
 
+def create_rectangle(point1: data.Point, point2: data.Point) -> data.Rectangle: #creates a rectangle given 2 of its points
+    #sets right and left
+    if point1.x > point2.x:
+        right = point1.x
+        left = point2.x
+    else:
+        right = point2.x
+        left = point1.x
+
+    #sets top and bottom
+    if point1.y > point2.y:
+        top = point1.y
+        bottom = point2.y
+    else:
+        top = point2.y
+        bottom = point1.y
+    
+    top_left = data.Point(left, top)
+    bottom_right = data.Point(right, bottom)
+
+    rectangle = data.Rectangle(top_left, bottom_right)
+
+    return rectangle
 
 # Part 2
 
+def shorter_duration_than(duration1: data.Duration, duration2: data.Duration) -> bool: #tests whether 1 given duration is shorter than another
+    total_seconds_1 = (duration1.minutes) * 60 + duration1.seconds
+    total_seconds_2 = (duration2.minutes) * 60 + duration2.seconds
+    return total_seconds_1 < total_seconds_2
 
 # Part 3
 
+def songs_shorter_than(songs: list[data.Song], max_length: data.Duration) -> list[data.Song]: #returns all songs shorter than a given duration from a given list of songs
+    return [] if songs == [] else [song for song in songs if shorter_duration_than(song.duration, max_length)]
 
 # Part 4
 
+def add_durations(durations: list[data.Duration]) -> data.Duration: #adds up a list of durations into one duration
+    if durations == []:
+        return 0
+    total_minutes = sum([duration.minutes for duration in durations])
+    total_seconds = sum([duration.seconds for duration in durations])
+    while total_seconds >= 60:
+        total_seconds -=60
+        total_minutes += 1
+    return data.Duration(total_minutes, total_seconds)
+
+def running_time(songs: list[data.Song], nums: list[int]) -> data.Duration: #returns the total running time of a playlist given a list of songs and an order
+    return 0 if songs == [] else add_durations([songs[num].duration for num in nums])
 
 # Part 5
 
+def validate_route(): #graph theory! kind of
+    pass
 
 # Part 6
--- hw2_tests.py
+++ hw2_tests.py
@@ -6,17 +6,101 @@ import unittest
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
+
     # Part 1
 
+    def test_create_rectangle_1(self):
+        point1 = data.Point(2, 2)
+        point2 = data.Point(10, 10)
+        result = hw2.create_rectangle(point1, point2)
+        expected = data.Rectangle(data.Point(2, 10), data.Point(10, 2))
+        self.assertEqual(result, expected)
+
+    def test_create_rectangle_2(self):
+        point1 = data.Point(7, -2)
+        point2 = data.Point(0, -2)
+        result = hw2.create_rectangle(point1, point2)
+        expected = data.Rectangle(data.Point(0, -2), data.Point(7, -2))
+        self.assertEqual(result, expected)
 
     # Part 2
+
+    def test_shorter_duration_than_1(self):
+        duration1 = data.Duration(5, 7)
+        duration2 = data.Duration(10, 0)
+        result = hw2.shorter_duration_than(duration1, duration2)
+        expected = True
+        self.assertEqual(result, expected)
+
+    def test_shorter_duration_than_2(self):
+        duration1 = data.Duration(10, 7)
+        duration2 = data.Duration(10, 0)
+        result = hw2.shorter_duration_than(duration1, duration2)
+        expected = False
+        self.assertEqual(result, expected)
 
 
     # Part 3
 
+    def test_songs_shorter_than_1(self):
+        length = data.Duration(3, 24)
+        song1 = data.Song('Artist', 'Title1', data.Duration(3, 21))
+        song2 = data.Song('Artist', 'Title2', data.Duration(3, 21))
+        song3 = data.Song('Artist', 'Title2', data.Duration(4, 21))
+        songs = [song1, song2, song3]
+        result = hw2.songs_shorter_than(songs, length)
+        expected = [song1, song2]
+        self.assertEqual(result, expected)
+    
+    def test_songs_shorter_than_2(self):
+        length = data.Duration(3, 0)
+        song1 = data.Song('Artist', 'Title1', data.Duration(3, 0))
+        song2 = data.Song('Artist', 'Title2', data.Duration(3, 1))
+        song3 = data.Song('Artist', 'Title2', data.Duration(4, 5))
+        songs = [song1, song2, song3]
+        result = hw2.songs_shorter_than(songs, length)
+        expected = []
+        self.assertEqual(result, expected)
+
+    def test_songs_shorter_than_3(self):
+        length = data.Duration(3, 0)
+        songs = []
+        result = hw2.songs_shorter_than(songs, length)
+        expected = []
+        self.assertEqual(result, expected)
 
     # Part 4
 
+    def test_add_durations_1(self):
+        durations = [data.Duration(1, 1), data.Duration(7, 16)]
+        result = hw2.add_durations(durations)
+        expected = data.Duration(8, 17)
+        self.assertEqual(expected, result)
+
+    def test_add_durations_2(self):
+        durations = [data.Duration(1, 59), data.Duration(7, 2)]
+        result = hw2.add_durations(durations)
+        expected = data.Duration(9, 1)
+        self.assertEqual(expected, result)
+
+    def test_add_durations_3(self):
+        durations = []
+        result = hw2.add_durations(durations)
+        expected = 0
+        self.assertEqual(expected, result)
+
+
+    def test_running_time_1(self):
+        songs = [data.Song("Me", "Me", data.Duration(4, 30)), data.Song("Me", "Me", data.Duration(3, 40))]
+        result = hw2.running_time(songs, [0, 1, 0])
+        expected = data.Duration(12, 40)
+        self.assertEqual(expected, result)
+
+    def test_running_time_2(self):
+        songs = []
+        result = hw2.running_time(songs, [0])
+        expected = 0
+        self.assertEqual(expected, result)
 
     # Part 5
 
